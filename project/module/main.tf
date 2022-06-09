abfee2f2087ccc0701ecde67a14f49ceb98c1c88
Kyial <kikuli.1992@gmail.com>
2022-06-09 06:44:05 +0000

    added README

--- project/module/main.tf
+++ project/module/main.tf
@@ -15,3 +15,15 @@ module "vpc" {
 
 
 }
+output vpc_id {
+    value = module.vpc.vpc_id
+}
+output public_subnet1 {
+    value = module.vpc.public_subnet1
+}
+output public_subnet2 {
+    value = module.vpc.public_subnet2
+}
+output public_subnet3 {
+    value = module.vpc.public_subnet3
+}
