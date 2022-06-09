52dd9aeaac3f1688e365d39203e3b19564d6d4d4
Kyial <kikuli.1992@gmail.com>
2022-06-09 00:02:13 +0000

    add bash localscript.sh

--- localscript.sh
+++ localscript.sh
@@ -2,3 +2,6 @@
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
 sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+
+#bash localscript.sh
+tfenv
