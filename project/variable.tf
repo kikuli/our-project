c141b0c9596fd9137e108368cd3a8b66e101a5f9
Kyial <kikuli.1992@gmail.com>
2022-06-09 03:00:31 +0000

    add subnets

--- project/variable.tf
+++ project/variable.tf
@@ -8,3 +8,13 @@ variable "region" {
   type        = string
   default     = ""
 }
+variable "tags" {
+  description = "Please provide a tag for resources"
+  type        = map(any)
+  default     = {}
+}
+variable "public_subnet1" {
+  description = "Please provide subnet CIDR block"
+  type        = string
+  default     = ""
+}
