4b4898fc6634a81d42395be98a0505117493b6af
kyial2992 <kyial2992@gmail.com>
2022-06-09 04:21:46 +0000

    added private

--- project/main.tf
+++ project/main.tf
@@ -28,28 +28,6 @@ resource "aws_subnet" "public3" {
 
 }
 
-resource "aws_subnet" "public1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet1
-  tags                    = var.tags
-  map_public_ip_on_launch = true
-
-}
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet2
-  tags                    = var.tags
-  map_public_ip_on_launch = true
-
-}
-resource "aws_subnet" "public3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet3
-  tags                    = var.tags
-  map_public_ip_on_launch = true
-
-}
-
 resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet1
